498f097278ea3bb2ed567d32f6ec8778b98ee437
mike <liuqiao@alfeye.com>
2021-03-30 09:31:32 +0800

    Remove class @Component on DynamicPropertiesBeanAutoConfiguration.java

--- src/main/java/com/github/selfancy/apollo/DynamicPropertiesBeanAutoConfiguration.java
+++ src/main/java/com/github/selfancy/apollo/DynamicPropertiesBeanAutoConfiguration.java
@@ -1,14 +1,12 @@
 package com.github.selfancy.apollo;
 
 import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Component;
 
 /**
  * DynamicPropertiesBeanAutoConfiguration
  * <p>
  * Created by mike on 2020/10/27 since 1.0
  */
-@Component
 @Import({DynamicPropertiesConfigBeanPostProcessor.class, DynamicPropertiesChangeBinderListener.class})
 class DynamicPropertiesBeanAutoConfiguration {
 }
